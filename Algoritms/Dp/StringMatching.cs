217c035de3f174bca43fca2bcc5814436104c43a
Gowtham <gowtham.b@tripstack.com>
2023-04-13 14:19:40 +0530

    Dp algoritm one

---
+++ Algoritms/Dp/StringMatching.cs
@@ -0,0 +1,36 @@
+namespace Algoritms.Dp;
+
+public class StringMatching
+{
+    public int MinimumChangesToMAkeStringSame(string one, string two)
+    {
+        int[,] dp = new int[one.Length+1,two.Length+1];
+
+        for (int i = 0; i < one.Length; i++)
+        {
+            for (int j = 0; j < two.Length; j++)
+            {
+                if (i == 0)
+                {
+                    dp[i,j] = j;
+                }
+
+                else if (j == 0)
+                {
+                    dp[i, j] = i;
+                }
+
+                else if (one.ToCharArray()[i] == two.ToCharArray()[j])
+                {
+                    dp[i, j] = dp[i - 1, j - 1];
+                }
+                else
+                {
+                    dp[i, j] = 1 + Math.Min(dp[i - 1, j], Math.Min(dp[i, j - 1], dp[i - 1, j - 1]));
+                }
+            }
+        }
+
+        return dp[one.Length, two.Length];
+    }
+}

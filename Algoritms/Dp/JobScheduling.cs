af4a982d8b3d2ec40f46f2f6337eec988ebaeb87
Gowtham <gowtham.b@tripstack.com>
2023-04-13 17:52:31 +0530

    Dp algoritms mostly done

--- Algoritms/Dp/JobScheduling.cs
+++ Algoritms/Dp/JobScheduling.cs
@@ -18,6 +18,7 @@ public class JobScheduling
                         dp[i] = dp[j] + profit[i];
                     }
                 }
+                
             }
             //find the max
     }
--- Algoritms/Dp/LongestCommonSubString.cs
+++ Algoritms/Dp/LongestCommonSubString.cs
@@ -6,13 +6,13 @@ public class LongestCommonSubString
     {
         string a = "something";
         string b = "anything";
-        var dp = new int[a.Length+1, b.Length+1];
+        var dp = new int[a.Length + 1, b.Length + 1];
         int max = int.MinValue;
         for (int i = 1; i <= a.Length; i++)
         {
             for (int j = 1; j <= b.Length; j++)
             {
-                if (a.ToCharArray()[i-1] == b.ToCharArray()[j-1])
+                if (a.ToCharArray()[i - 1] == b.ToCharArray()[j - 1])
                 {
                     dp[i, j] = 1 + dp[i - 1, j - 1];
                     if (dp[i, j] > max)
@@ -32,21 +32,22 @@ public class LongestCommonSubString
     {
         string a = "something";
         string b = "anything";
-        var dp = new int[a.Length+1, b.Length+1];
+        var dp = new int[a.Length + 1, b.Length + 1];
         int max = int.MinValue;
         for (int i = 1; i <= a.Length; i++)
         {
             for (int j = 1; j <= b.Length; j++)
             {
-                if (a.ToCharArray()[i-1] == b.ToCharArray()[j-1])
+                if (a.ToCharArray()[i - 1] == b.ToCharArray()[j - 1])
                 {
                     dp[i, j] = 1 + dp[i - 1, j - 1];
-                   
+
                 }
                 else
                 {
-                    dp[i, j] = Math.Max(dp[i-1,j],dp[i,j-1]);
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
                 }
             }
+        }
     }
 }
